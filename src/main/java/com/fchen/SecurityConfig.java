6959094a0015c0d6bc6c6c7a88700d5e86df901b
Chen Longmin <chenlongmin@zhixuezhen.com>
2019-06-24 14:40:39 +0800

    提交代码

--- src/main/java/com/fchen/SecurityConfig.java
+++ src/main/java/com/fchen/SecurityConfig.java
@@ -4,36 +4,51 @@ import com.fchen.repository.ReaderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configurers.provisioning.InMemoryUserDetailsManagerConfigurer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 /**
  * Created by Fiona on 2018/6/27.
  */
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(
+        prePostEnabled = true,
+        securedEnabled = true,
+        jsr250Enabled = true
+)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    @Autowired
-    private ReaderRepository readerRepository;
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(new UserDetailsService() {
-                    @Override
-                    public UserDetails loadUserByUsername(String username)
-                            throws UsernameNotFoundException {
-                        return readerRepository.findByUsername(username);
-                    }
-                });
-    }
+//    @Autowired
+//    private ReaderRepository readerRepository;
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.userDetailsService(username -> readerRepository.findByUsername(username));
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/").access("hasRole('READER')").antMatchers("/**").permitAll()
-                .and().formLogin().loginPage("/login").failureUrl("/login?error=true");
+        http.authorizeRequests()
+                .antMatchers("/", "/addUser", "/success").permitAll()
+                .anyRequest().authenticated()
+                .and().formLogin().loginPage("/login").failureUrl("/login?error=true").permitAll();
+
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        ((InMemoryUserDetailsManagerConfigurer)auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder())).withUser("lg2x").password((new BCryptPasswordEncoder()).encode("lg2xlqxly")).roles(new String[]{"USER"});
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(new String[]{"/css/**", "/image/**", "/js/**"});
     }
 }
